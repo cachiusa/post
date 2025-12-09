ccf11011a8eae0895dc5ec3e86cfe27ebaed6d42
Nhat <nhat.dogpro@outlook.com>
2025-12-09 13:32:47 +0000

    use new layout as const

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,6 +13,18 @@ export const sharedPageComponents: SharedLayout = {
   }),
 }
 
+export const customLeftLayout = [
+  Component.DesktopOnly(Component.PageTitle()),
+  Component.Search(),
+  Component.Flex({
+    components: [
+      { Component: Component.Darkmode() },
+      { Component: Component.ReaderMode() },
+    ],
+  }),
+  Component.Explorer(),
+]
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -24,17 +36,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
-    Component.DesktopOnly(Component.PageTitle()),
-    Component.Search(),
-    Component.Flex({
-      components: [
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
-  ],
+  left: customLeftLayout,
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -44,20 +46,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-      ],
-    }),
-    Component.Explorer(),
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta()
   ],
+  left: customLeftLayout,
   right: [],
 }
